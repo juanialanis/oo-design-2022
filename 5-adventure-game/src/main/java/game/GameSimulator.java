9fef19e8afd21163b9175b5af66a54845e4f06ef
Federico Gutierrez <fede.a.gutierrez1@gmail.com>
2022-10-23 20:33:05 -0300

    adicion de los archivos faltantes del game simulation

--- 5-adventure-game/src/main/java/game/GameSimulator.java
+++ 5-adventure-game/src/main/java/game/GameSimulator.java
@@ -3,11 +3,13 @@ package game;
 public class GameSimulator {
  
 	public static void main(String[] args) {
-    Character char1 = new Knight();
-    Character char2 = new Wizard();
-    char2.setWeapon(new Wand());
-		Fight fight = new Fight(char1, char2);
-    fight.fight();
+    Game g = new Game();
+    GameConfiguration config = g.getConfig();
+    Character knight = new Knight();
+    knight.setWeapon(new Mace());
+    config.setPlayer1Character(knight);
+    config.setPlayer2Character(new Elf());
+    g.play();
 	}
 
 }
