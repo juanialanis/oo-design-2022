051dd096991bd8abef371bf49f7fb61478da0338
Juan Alanis <juanito_alanis@hotmail.com>
2022-11-28 00:31:03 -0300

    test en el 9 b

--- 9-n-primes-calculator/src/main/java/calculator/CalculatorEratosthenes.java
+++ 9-n-primes-calculator/src/main/java/calculator/CalculatorEratosthenes.java
@@ -1,10 +1,8 @@
 package calculator;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
